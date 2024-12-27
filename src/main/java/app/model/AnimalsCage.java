35a9d259be1e2d754afba7941d1a0cb01f3edb01
marfan <marfffan@gmail.com>
2024-12-27 19:55:11 +0300

    Отправка решения 26.12.24

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -19,7 +19,7 @@ public class AnimalsCage {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());/*Пришлось изменить код мейна чтобы счетчик не создавался каждый раз, но по другому тут не как. Хотя можно было просто и в задании написать что докажите мол что таймер singltone*/
         System.out.println("________________________");
     }
 
